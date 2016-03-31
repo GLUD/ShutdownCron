1413756e16110b45d884e1e0087bf277e7f54e55
Jorge Useche <naturalmentejorge@gmail.com>
2016-03-31 17:53:41 -0500

    Se agregan mensajes para avisar al usuario sobre el apagon

--- shutdown.sh
+++ shutdown.sh
@@ -1,5 +1,15 @@
 #!/bin/bash
 #Compruebe con el comando
 # $ locate poweroff
-#Que la aplicación poweroff se encuenta en esta dirección:
+#Que la aplicación poweroff se encuenta en /sbin/poweroff
+#además que el .Xauthority sea de un usuario que tenga sesión gráfica
+#por último que la pantalla predeterminada sea :0 con el comando who
+export XAUTHORITY=/home/glud/.Xauthority
+export DISPLAY=:0
+for i in `seq 1 5`; do
+let min=6-$i
+xmessage -center "Guarde sus trabajos, el computador se apagara en $min min" &
+echo "Guarde sus trabajos"
+sleep 60
+done
 /sbin/poweroff
